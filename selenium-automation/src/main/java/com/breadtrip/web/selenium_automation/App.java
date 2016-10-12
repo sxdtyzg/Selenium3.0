2209e7e9d9d11043b188092d9d54561c941c2a81
YZG <yangzhiguang@breadtrip.com>
2016-10-12 17:58:02 +0800

    New maven project

---
+++ selenium-automation/src/main/java/com/breadtrip/web/selenium_automation/App.java
@@ -0,0 +1,13 @@
+package com.breadtrip.web.selenium_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
