bd6afbe49f7e4944c26b163b1f46a2036a8ae084
YZG <yangzhiguang@breadtrip.com>
2016-10-17 17:53:34 +0800

    Test

--- selenium-automation/src/main/java/com/breadtrip/web/selenium_automation/TestSearch.java
+++ selenium-automation/src/main/java/com/breadtrip/web/selenium_automation/TestSearch.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class TestLogin {
+public class TestSearch {
 	public static void main(String[] args) {
-//		System.setProperty("webdriver.chrome.driver", "/Users/yzg/Downloads/chromedriver");
-		System.setProperty("webdriver.chrome.driver", "F:/Computer/Tool/Workspace/Selenium/selenium-automation/BrowserDriver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/Users/yzg/Downloads/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "F:/Computer/Tool/Workspace/Selenium/selenium-automation/BrowserDriver/chromedriver.exe");
 		
 		//WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
-		Login lg = PageFactory.initElements(driver, Login.class);
-		lg.login(driver,"18710002864", "xxxxxx");
+		Search lg = PageFactory.initElements(driver, Search.class);
+		lg.search(driver,"testng");
 	}
 }
